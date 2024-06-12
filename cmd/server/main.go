6d5ed4b86f649a2561103e1012dbc81bee823411
matheusvidal <matheusvidal140@gmail.com>
2024-06-12 17:23:45 -0300

    Criando as rotas

---
+++ cmd/server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/matheusvidal21/crud-go/src/controller/routes"
+	"log"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+	router := gin.Default()
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
+}
---
+++ src/controller/findUser.go
@@ -0,0 +1,11 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func FindUserById(c *gin.Context) {
+	// code here
+}
+
+func FindUserByEmail(c *gin.Context) {
+
+}
